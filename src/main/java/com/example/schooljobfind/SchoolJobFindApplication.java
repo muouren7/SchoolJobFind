b294a3be2cbbb8dbec0712073b0e3ac655f6f123
muouren7 <1744811259@qq.com>
2023-03-26 22:10:41 +0800

    初始化项目

---
+++ src/main/java/com/example/schooljobfind/SchoolJobFindApplication.java
@@ -0,0 +1,13 @@
+package com.example.schooljobfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolJobFindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolJobFindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schooljobfind/SchoolJobFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schooljobfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolJobFindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
